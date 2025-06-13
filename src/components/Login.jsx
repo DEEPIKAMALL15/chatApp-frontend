07ce770e3d5808fed27eef846a478b45dc26de97
DEEPIKAMALL15 <deepikam1508@gmail.com>
2025-06-14 00:55:22 +0530

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
  import React , {useState} from 'react'
-import { Link, useNavigate } from 'react-router'
+import { Link, useNavigate } from 'react-router-dom'
 import toast from "react-hot-toast"
 import axios from "axios"
 import { useDispatch } from 'react-redux'
