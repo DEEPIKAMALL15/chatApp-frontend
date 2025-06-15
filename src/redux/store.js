acf71976326aeb1cfc887a3d7d1404e2999ef977
DEEPIKAMALL15 <deepikam1508@gmail.com>
2025-06-15 19:27:48 +0530

    socket

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import {combineReducers, configureStore}  from "@reduxjs/toolkit";
+/* import {combineReducers, configureStore}  from "@reduxjs/toolkit";
 import userReducer from "./userSlice.js";
 import messageReducer from "./messageSlice.js"
 import socketReducer from "./socketSlice"
@@ -34,6 +34,50 @@ const store = configureStore ({
           ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
         },
       }),
+});
+
+export default store;
+ */
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice.js";
+import messageReducer from "./messageSlice.js";
+import socketReducer from "./socketSlice";
+
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: ['socket'], // 🛑 Don't persist socket slice
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  message: messageReducer,
+  socket: socketReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;
